c67a393ded0b2caf67b544a9c67b35541b6f7404
hubalexander <har14ape@student.lu.se>
2021-05-11 20:53:00 +0200

    Updated css and readme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import './css/Search.css';
+import './css/Button.css';
 import React,{useState, useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  
+
+//Hooks
 const [search, setSearch] = useState('');
 const [creatures, setCreatures] = useState([]);
 const [filteredCreatures, setFilteredCreatures] = useState([]);
 const [selectedCreatures, setSelectedCreatures] = useState([]);
 
-
+//Fetches the JSON data from the Pokémon API. 
 useEffect(() => {
   axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=100")
@@ -18,6 +20,7 @@ useEffect(() => {
       });
 }, []);
 
+//Filters the fetched data based on the input in the search box. 
 useEffect(() => {
   setFilteredCreatures(
       creatures.filter((creature) =>
@@ -26,13 +29,12 @@ useEffect(() => {
   );
 }, [search, creatures]);
 
+  //Rendering 
   return (
-    
     <div  className="container">
     
-        <button className="RemoveListButton" onClick={()=>{setSelectedCreatures([])}}>Remove list</button>  
-    
-        <button className="RemoveSearchButton" onClick={()=>{setSearch('')}}>Empty search</button>    
+        <button className="removeListButton" onClick={()=>{setSelectedCreatures([])}}>Remove list</button>  
+        <button className="removeSearchButton" onClick={()=>{setSearch('')}}>Empty search</button>    
     
         <div>
             <input className="searchBox" type="text"  placeholder="Search for a Pokémon by name" value={search} style={{marginTop:10}}
@@ -40,12 +42,12 @@ useEffect(() => {
         </div>
 
         {search ? (
-            <div className="ResultList">
+            <div className="resultList">
                 
                     {filteredCreatures.slice(0,10).map((value)=>{
                     return <div key={Math.random().toString(36).substr(2, 9)} tabIndex="0">
       
-            <div  className="SearchSuggestion"  onClick={()=>{
+            <div  className="searchSuggestion"  onClick={()=>{
               setSelectedCreatures((previousArray) => ([...previousArray, value.name + "@" + Date().slice(16,25)]))}}>{value.name}</div>
     
             </div>
@@ -53,11 +55,13 @@ useEffect(() => {
           </div> ) : null}
 
           {selectedCreatures ? (
-          <div className="SavedList"> {selectedCreatures}</div>
+          <div className="savedList"> {selectedCreatures}</div>
           ) : null}
           
       </div>
     );
 }
+
+
 
 export default App;
