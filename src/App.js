80bc1f046a6a809ae023d1b9e518adfddec0e026
hubalexander <har14ape@student.lu.se>
2021-05-11 20:25:36 +0200

    Api call and basic design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+const [search, setSearch] = useState('');
+const [creatures, setCreatures] = useState([]);
+const [filteredCreatures, setFilteredCreatures] = useState([]);
+const [selectedCreatures, setSelectedCreatures] = useState([]);
+
+
+useEffect(() => {
+  axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=100")
+      .then((response) => {
+          setCreatures(response.data.results);
+      });
+}, []);
+
+useEffect(() => {
+  setFilteredCreatures(
+      creatures.filter((creature) =>
+          creature.name.toLowerCase().includes(search.toLowerCase())
+      )
   );
+}, [search, creatures]);
+
+  return (
+    
+    <div  className="container">
+    
+        <button className="RemoveListButton" onClick={()=>{setSelectedCreatures([])}}>Remove list</button>  
+    
+        <button className="RemoveSearchButton" onClick={()=>{setSearch('')}}>Empty search</button>    
+    
+        <div>
+            <input className="searchBox" type="text"  placeholder="Search for a Pokémon by name" value={search} style={{marginTop:10}}
+            onChange={(e) => {setSearch(e.target.value)}}/>      
+        </div>
+
+        {search ? (
+            <div className="ResultList">
+                
+                    {filteredCreatures.slice(0,10).map((value)=>{
+                    return <div key={Math.random().toString(36).substr(2, 9)} tabIndex="0">
+      
+            <div  className="SearchSuggestion"  onClick={()=>{
+              setSelectedCreatures((previousArray) => ([...previousArray, value.name + "@" + Date().slice(16,25)]))}}>{value.name}</div>
+    
+            </div>
+            })}
+          </div> ) : null}
+
+          {selectedCreatures ? (
+          <div className="SavedList"> {selectedCreatures}</div>
+          ) : null}
+          
+      </div>
+    );
 }
 
 export default App;
